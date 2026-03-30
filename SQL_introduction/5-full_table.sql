803b2a345592fd0207dfcff7ba517d9fb5176557
Jale03 <jale.qocayeva.dev@outlook.com>
2026-03-30 16:42:22 +0000

    Task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Displays the number of records with id = 89 in the table first_table
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+-- Prints the full description of the table first_table
+SHOW CREATE TABLE first_table;
