3d378189a1a2a49e4441b17ef421445e5d86eaef
Jale03 <jale.qocayeva.dev@outlook.com>
2026-04-04 10:10:10 +0000

    sdfg

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,13 +12,10 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
-
     cur.close()
     db.close()
